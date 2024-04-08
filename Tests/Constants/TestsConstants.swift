48f36a101162db451f246d1afc8b1c3d84999457
Dimitris Tsiflitzis <dimitris.tsiflitzis@netcompany-intrasoft.com>
2024-04-08 11:43:20 +0300

    [fix] updated unit tests constants

--- Tests/Constants/TestsConstants.swift
+++ Tests/Constants/TestsConstants.swift
@@ -152,7 +152,7 @@ struct TestsConstants {
     
     let credentialOfferRequestResolver = CredentialOfferRequestResolver(
       fetcher: Fetcher<CredentialOfferRequestObject>(session: NetworkingMock(
-        path: "credential_offer_with_blank_pre_authorized_code",
+        path: "credential_offer_with_blank_issuer_state",
         extension: "json"
       )),
       credentialIssuerMetadataResolver: credentialIssuerMetadataResolver,
@@ -168,6 +168,39 @@ struct TestsConstants {
     let credentialIssuerMetadataResolver = CredentialIssuerMetadataResolver(
       fetcher: Fetcher<CredentialIssuerMetadata>(session: NetworkingMock(
         path: "openid-credential-issuer_no_encryption",
+        extension: "json"
+      )
+    ))
+    
+    let authorizationServerMetadataResolver = AuthorizationServerMetadataResolver(
+      oidcFetcher: Fetcher<OIDCProviderMetadata>(session: NetworkingMock(
+        path: "oidc_authorization_server_metadata",
+        extension: "json"
+      )),
+      oauthFetcher: Fetcher<AuthorizationServerMetadata>(session: NetworkingMock(
+        path: "test",
+        extension: "json"
+      ))
+    )
+    
+    let credentialOfferRequestResolver = CredentialOfferRequestResolver(
+      fetcher: Fetcher<CredentialOfferRequestObject>(session: NetworkingMock(
+        path: "credential_offer_with_blank_pre_authorized_code",
+        extension: "json"
+      )),
+      credentialIssuerMetadataResolver: credentialIssuerMetadataResolver,
+      authorizationServerMetadataResolver: authorizationServerMetadataResolver
+    )
+    
+    return try? await credentialOfferRequestResolver.resolve(
+      source: .fetchByReference(url: .stub())
+    ).get()
+  }
+  
+  static func createMockPreAuthCredentialOffer() async -> CredentialOffer? {
+    let credentialIssuerMetadataResolver = CredentialIssuerMetadataResolver(
+      fetcher: Fetcher<CredentialIssuerMetadata>(session: NetworkingMock(
+        path: "credential_issuer_metadata",
         extension: "json"
       )
     ))
