dbb90f4e3e89b09a8efaa5aa84e551fe50afac48
Dimitris Tsiflitzis <dimitris.tsiflitzis@netcompany-intrasoft.com>
2024-05-23 09:34:38 +0300

    [fix] updated test constants with new config

--- Tests/Constants/TestsConstants.swift
+++ Tests/Constants/TestsConstants.swift
@@ -77,7 +77,7 @@ let MDL_CredentialOffer = """
     }
 """
 
-let config: WalletOpenId4VCIConfig = .init(
+let config: OpenId4VCIConfig = .init(
   clientId: "wallet-dev",
   authFlowRedirectionURI: URL(string: "urn:ietf:wg:oauth:2.0:oob")!, 
   authorizeIssuanceConfig: .favorScopes
