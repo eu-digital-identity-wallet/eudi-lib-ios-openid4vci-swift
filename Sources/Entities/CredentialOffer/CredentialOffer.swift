6b004d3de4836c27c68d24f487d14ec1a4f889a1
dtsiflit <dimitri.tsiflitzis@netcompany.com>
2025-05-15 15:35:37 +0300

    [fix] authorization oprimisations in entities

--- Sources/Entities/CredentialOffer/CredentialOffer.swift
+++ Sources/Entities/CredentialOffer/CredentialOffer.swift
@@ -39,6 +39,16 @@ public struct CredentialOffer: Sendable {
     if credentialConfigurationIdentifiers.isEmpty {
       throw CredentialOfferRequestError.emptyCredentialsError
     }
+    
+    switch grants {
+    case .authorizationCode(let code):
+      if code.authorizationServer == nil {
+        throw CredentialOfferRequestError.nonParseableCredentialOffer(
+          reason: "Grant: nil authorizationServer"
+        )
+      }
+    default: break
+    }
   }
 }
 
---
+++ Sources/Entities/Issuance/AuthorizationRequestPrepared.swift
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2023 European Commission
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import Foundation
+
+/// State denoting that the pushed authorization request has been placed successfully and response processed
+public struct AuthorizationRequested: Sendable {
+  public let credentials: [CredentialIdentifier]
+  public let authorizationCodeURL: AuthorizationCodeURL
+  public let pkceVerifier: PKCEVerifier
+  public let state: String
+  public let configurationIds: [CredentialConfigurationIdentifier]
+  public let dpopNonce: Nonce?
+  
+  public init(
+    credentials: [CredentialIdentifier],
+    authorizationCodeURL: AuthorizationCodeURL,
+    pkceVerifier: PKCEVerifier,
+    state: String,
+    configurationIds: [CredentialConfigurationIdentifier],
+    dpopNonce: Nonce? = nil
+  ) {
+    self.credentials = credentials
+    self.authorizationCodeURL = authorizationCodeURL
+    self.pkceVerifier = pkceVerifier
+    self.state = state
+    self.configurationIds = configurationIds
+    self.dpopNonce = dpopNonce
+  }
+}
+
+/// State denoting that caller has followed the GetAuthorizationCodeURL URL and response received
+/// from the authorization server and processed successfully.
+public struct AuthorizationCodeRetrieved: Sendable {
+  public let credentials: [CredentialIdentifier]
+  public let authorizationCode: IssuanceAuthorization
+  public let pkceVerifier: PKCEVerifier
+  public let configurationIds: [CredentialConfigurationIdentifier]
+  public let dpopNonce: Nonce?
+  
+  public init(
+    credentials: [CredentialIdentifier],
+    authorizationCode: IssuanceAuthorization,
+    pkceVerifier: PKCEVerifier,
+    configurationIds: [CredentialConfigurationIdentifier],
+    dpopNonce: Nonce?
+  ) throws {
+    
+    guard case .authorizationCode = authorizationCode else {
+      throw ValidationError.error(reason: "IssuanceAuthorization must be authorization code")
+    }
+
+    self.credentials = credentials
+    self.authorizationCode = authorizationCode
+    self.pkceVerifier = pkceVerifier
+    self.configurationIds = configurationIds
+    self.dpopNonce = dpopNonce
+  }
+}
+
+public enum AuthorizationRequestPrepared: Sendable {
+  case prepared(AuthorizationRequested)
+  case authorizationCode(AuthorizationCodeRetrieved)
+}
