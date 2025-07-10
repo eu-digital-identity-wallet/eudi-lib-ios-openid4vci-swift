c542bfac06b36bd41813a823951b3cd09b152419
dtsiflit <dimitri.tsiflitzis@netcompany.com>
2025-07-10 14:26:27 +0300

    [fix] tests foxes for key attestation

---
+++ Tests/AttestationBased/KeyAttestationTests.swift
@@ -0,0 +1,191 @@
+/*
+ * Copyright (c) 2023 European Commission
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import Foundation
+import XCTest
+import JOSESwift
+
+@testable import OpenID4VCI
+
+class KeyAttestationTests: XCTestCase {
+  
+  let config: OpenId4VCIConfig = .init(
+    client: .public(id: "wallet-dev"),
+    authFlowRedirectionURI: URL(string: "urn:ietf:wg:oauth:2.0:oob")!,
+    authorizeIssuanceConfig: .favorScopes
+  )
+  
+  override func setUp() async throws {
+    try await super.setUp()
+  }
+  
+  override func tearDown() {
+    super.tearDown()
+  }
+  
+  func testWhenIssuerDemandsKeyAttestationShouldBeIncludedInJWTProof() async throws {
+    
+    // Given
+    guard let offer = await TestsConstants.createMockCredentialOfferopenidKeyAttestationRequired() else {
+      XCTAssert(false, "Unable to resolve credential offer")
+      return
+    }
+    
+    // Given
+    let privateKey = try KeyController.generateECDHPrivateKey()
+    let publicKey = try KeyController.generateECDHPublicKey(from: privateKey)
+    
+    let alg = JWSAlgorithm(.ES256)
+    let publicKeyJWK = try ECPublicKey(
+      publicKey: publicKey,
+      additionalParameters: [
+        "alg": alg.name,
+        "use": "enc",
+        "kid": UUID().uuidString
+      ])
+    
+    let spec = IssuanceResponseEncryptionSpec(
+      jwk: publicKeyJWK,
+      privateKey: privateKey,
+      algorithm: .init(.ECDH_ES),
+      encryptionMethod: .init(.A128CBC_HS256)
+    )
+    
+    // When
+    let issuer = try Issuer(
+      authorizationServerMetadata: offer.authorizationServerMetadata,
+      issuerMetadata: offer.credentialIssuerMetadata,
+      config: config,
+      parPoster: Poster(
+        session: NetworkingMock(
+          path: "pushed_authorization_request_response",
+          extension: "json"
+        )
+      ),
+      tokenPoster: Poster(
+        session: NetworkingMock(
+          path: "access_token_request_response_no_proof",
+          extension: "json"
+        )
+      ),
+      requesterPoster: Poster(
+        session: NetworkingMock(
+          path: "batch_credential_issuance_success_response_credentials",
+          extension: "json"
+        )
+      ),
+      noncePoster: Poster(
+        session: NetworkingMock(
+          path: "mock_cnonce_endpoint_response",
+          extension: "json"
+        )
+      ),
+      dpopConstructor: dpopConstructor(
+        algorithms: offer.authorizationServerMetadata.dpopSigningAlgValuesSupported
+      )
+    )
+    
+    let authorizationCode = "MZqG9bsQ8UALhsGNlY39Yw=="
+    let request: AuthorizationRequestPrepared = TestsConstants.unAuthorizedRequest
+    
+    let issuanceAuthorization: IssuanceAuthorization = .authorizationCode(authorizationCode: authorizationCode)
+    let unAuthorized = await issuer.handleAuthorizationCode(
+      request: request,
+      authorizationCode: issuanceAuthorization
+    )
+    
+    switch unAuthorized {
+    case .success(let authorizationCode):
+      let authorizedRequest = await issuer.authorizeWithAuthorizationCode(request: authorizationCode)
+      
+      if case let .success(authorized) = authorizedRequest {
+        XCTAssert(true, "Got access token: \(authorized.accessToken)")
+        XCTAssert(true, "Is no proof required")
+        
+        do {
+                    
+          let sdJwtVCpayload: IssuanceRequestPayload = .configurationBased(
+            credentialConfigurationIdentifier: try .init(value: "eu.europa.ec.eudiw.pid_vc_sd_jwt")
+          )
+          
+          let keyBindingKey: BindingKey = try! .keyAttestation(
+            algorithm: .init(.ES256),
+            keyAttestationJWT: .init(
+              jws: .init(
+                compactSerialization: TestsConstants.ketAttestationJWT
+              )
+            ),
+            keyIndex: 1,
+            privateKey: .secKey(privateKey),
+            issuer: "https://issuer.example.com"
+          )
+          
+          let altBindingKey: BindingKey = .jwk(
+            algorithm: .init(.ES256),
+            jwk: publicKeyJWK,
+            privateKey: .secKey(privateKey),
+            issuer: "https://issuer.example.com"
+          )
+          
+          let result = try await issuer.requestCredential(
+            request: authorized,
+            bindingKeys: [
+              altBindingKey,
+              keyBindingKey
+            ],
+            requestPayload: sdJwtVCpayload,
+            responseEncryptionSpecProvider: { _ in
+              spec
+            })
+          
+          switch result {
+          case .success(let request):
+            switch request {
+            case .success(let response):
+              if let result = response.credentialResponses.first {
+                switch result {
+                case .deferred:
+                  XCTAssert(false, "Unexpected deferred")
+                case .issued(_, let credential, _, _):
+                  XCTAssert(true, "credential: \(credential)")
+                  return
+                }
+              } else {
+                break
+              }
+            case .failed(let error):
+              XCTAssert(false, error.localizedDescription)
+              
+            case .invalidProof(let errorDescription):
+              XCTAssert(false, errorDescription!)
+            }
+            XCTAssert(false, "Unexpected request")
+          case .failure(let error):
+            XCTAssert(false, error.localizedDescription)
+          }
+        } catch {
+          XCTAssert(false, error.localizedDescription)
+        }
+        
+        return
+      }
+      
+    case .failure(let error):
+      XCTAssert(false, error.localizedDescription)
+    }
+    
+    XCTAssert(false, "Unable to get access token")
+  }
+}
